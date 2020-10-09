878b9a23e3f9681e52fad14bd1ae3d7a4a6ef263
Justine Estanislao <jpatz.e99@gmail.com>
2020-10-09 19:38:23 -0400

    Second push of updated code

--- personal-budget/src/app/app-routing.module.ts
+++ personal-budget/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { P404Component } from './p404/p404.component';
 import { LoginComponent } from './login/login.component';
+import { HeroComponent } from './hero/hero.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
@@ -18,6 +21,18 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'hero',
+    component: HeroComponent
+  },
+  {
+    path: 'breadcrumbs',
+    component: BreadcrumbsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: '**',
